4939ff9a704efed521d4038fc138959964207729
Matt Lewis <domattthings@gmail.com>
2020-05-15 15:11:17 -0700

    Do not force compiling the request function.
    This requires users to have Mojito installed. Which makes this
    dependency no longer optional.

--- lib/client/mojito.ex
+++ lib/client/mojito.ex
@@ -3,11 +3,18 @@ defmodule Client.Mojito do
   @behaviour Http
   @compile {:inline, request: 1}
 
-  @impl true
-  def request(request_opts) do
-    case Mojito.request(request_opts) do
-      {:ok, %Mojito.Response{body: body}} -> {:ok, body}
-      {:error, any} -> {:error, any}
+  if match?({:module, _}, Code.ensure_compiled(Mojito)) do
+    @impl true
+    def request(request_opts) do
+      case Mojito.request(request_opts) do
+        {:ok, %Mojito.Response{body: body}} -> {:ok, body}
+        {:error, any} -> {:error, any}
+      end
+    end
+  else
+    @impl true
+    def request(_request_opts) do
+      {:error, :mojito_not_compiled}
     end
   end
 end
