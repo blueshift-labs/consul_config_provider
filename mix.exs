3d022b024b3cea5800353ffda2e21a072f647a1a
James Dalton <tarpdalton@users.noreply.github.com>
2021-09-02 13:43:47 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ConsulConfigProvider.MixProject do
   def project do
     [
       app: :consul_config_provider,
-      version: "0.2.0",
+      version: "0.2.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
